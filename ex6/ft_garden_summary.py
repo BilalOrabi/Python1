91a9f566296bbefeb3819b416b3943ed1cdd5e09
BilalOrabi <bilalorabi99@gmail.com>
2026-01-18 06:03:27 +0300

    commiting the first project in python

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,6 @@
+def ft_garden_summary() -> None:
+    garden_name: str = input("Enter garden name: ")
+    num_plants: int = int(input("Enter number of plants: "))
+    print(f"Garden: {garden_name}")
+    print(f"Plants: {num_plants}")
+    print("Status: Growing well!")
