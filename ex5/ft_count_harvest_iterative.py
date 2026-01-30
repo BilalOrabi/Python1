af5a1c30036f80cf9a45a8be498253c508b575cb
BilalOrabi <bilalorabi99@gmail.com>
2026-01-30 14:25:08 +0300

    deleting files

--- ex5/ft_count_harvest_iterative.py
+++
@@ -1,5 +0,0 @@
-def ft_count_harvest_iterative() -> None:
-    harvest_days: int = int(input("Days until harvest: "))
-    for current_day in range(1, harvest_days + 1):
-        print(f"Day {current_day}")
-    print("Harvest time!")
--- ex5/ft_count_harvest_recursive.py
+++
@@ -1,8 +0,0 @@
-def ft_count_harvest_recursive(day=1, harvest_days=None) -> None:
-    if harvest_days is None:
-        harvest_days: int = int(input("Days until harvest: "))
-    if day <= harvest_days:
-        print(f"Day {day}")
-        ft_count_harvest_recursive(day + 1, harvest_days)
-    else:
-        print("Harvest time!")
