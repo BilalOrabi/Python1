9267f1fd171958e52000cfe6d1219ae73ee3de4d
BilalOrabi <bilalorabi99@gmail.com>
2026-01-31 23:33:49 +0300

    finishing ex5

--- ft_plant_types.py
+++ ft_plant_types.py
@@ -4,8 +4,6 @@ class Plant:
         self._height = 0
         self._age = 0
         
-        print(f"Plant created: {self._name}")
-        
         if height is not None:
             self.set_height(height)
         if age is not None:
@@ -22,14 +20,12 @@ class Plant:
             self._security_error("height", height, "cm")
             return
         self._height = height
-        print(f"Height updated: {height}cm [OK]")
 
     def set_age(self, age):
         if age < 0:
             self._security_error("age", age, " days")
             return
         self._age = age
-        print(f"Age updated: {age} days [OK]")
 
     def get_height(self):
         return self._height
@@ -39,3 +35,80 @@ class Plant:
 
     def get_name(self):
         return self._name
+
+
+class Flower(Plant):
+    def __init__(self, name, height, age, color):
+        super().__init__(name, height, age)
+        self.color = color
+
+    def describe(self):
+        print(
+            f"{self._name} (Flower): "
+            f"{self._height}cm, {self._age} days, {self.color} color"
+        )
+
+    def bloom(self):
+        print(f"{self._name} is blooming beautifully!")
+
+
+class Tree(Plant):
+    def __init__(self, name, height, age, trunk_diameter):
+        super().__init__(name, height, age)
+        self.trunk_diameter = trunk_diameter
+
+    def describe(self):
+        print(
+            f"{self._name} (Tree): "
+            f"{self._height}cm, {self._age} days, "
+            f"{self.trunk_diameter}cm diameter"
+        )
+
+    def produce_shade(self):
+        shade = self.trunk_diameter * 1.56
+        print(
+            f"{self._name} provides "
+            f"{int(shade)} square meters of shade"
+        )
+
+
+class Vegetable(Plant):
+    def __init__(self, name, height, age, harvest_season, nutritional_value):
+        super().__init__(name, height, age)
+        self.harvest_season = harvest_season
+        self.nutritional_value = nutritional_value
+
+    def describe(self):
+        print(
+            f"{self._name} (Vegetable): "
+            f"{self._height}cm, {self._age} days, "
+            f"{self.harvest_season.lower()} harvest"
+        )
+
+    def nutrition_info(self):
+        print(
+            f"{self._name} is rich in {self.nutritional_value}"
+        )
+
+def main():
+    print("=== Garden Plant Types ===\n")
+
+    rose = Flower("Rose", 25, 30, "red")
+    rose.describe()
+    rose.bloom()
+
+    print()
+
+    oak = Tree("Oak", 500, 1825, 50)
+    oak.describe()
+    oak.produce_shade()
+
+    print()
+
+    tomato = Vegetable("Tomato", 80, 90, "Summer", "vitamin C")
+    tomato.describe()
+    tomato.nutrition_info()
+
+
+if __name__ == "__main__":
+	main()
